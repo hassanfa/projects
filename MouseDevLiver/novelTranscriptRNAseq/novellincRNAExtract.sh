616c5e7caa3c11e2872796e27e04af9dc34791a2
Hassan Foroughi Asl <hassanfa@tintin3.uppmax.uu.se>
2017-01-18 13:31:37 +0100

    removed filtering of single exon novel transcripts

--- MouseDevLiver/novelTranscriptRNAseq/novellincRNAExtract.sh
+++ MouseDevLiver/novelTranscriptRNAseq/novellincRNAExtract.sh
@@ -5,15 +5,15 @@
 
 ./awkExtractNovelGene.awk \
   merged_asm_novelonly/merged.gtf \
-  | awk '$3-$2>199 && $5>1' > merged_asm_novelonly/lincRNA.candidate
+  | awk '$3-$2>199' > merged_asm_novelonly/lincRNA.candidate
 
 #list of genes that are fitlered above are then selected from merged.gtf file.
 join -1 1 -2 10 \
   <( cut -f 4 merged_asm_novelonly/lincRNA.candidate | sort) \
   <( cat merged_asm_novelonly/merged.gtf | tr ' ' '\t' | sort -k10) \
-  | awk '{for (i=2; i<11; i++)
+  | awk '{for (i=2; i<10; i++)
             printf $i"\t";
-          printf $1" ";
-          for (i=11; i<=18; i++) 
+          printf $10" "$1" ";
+          for (i=11; i<=17; i++) 
             printf $i" ";
-          printf "\n"}' > merged_asm_novelonly/novellincRNA.candidate.gtf 
+          printf $18"\n"}' > merged_asm_novelonly/novellincRNA.candidate.gtf 
