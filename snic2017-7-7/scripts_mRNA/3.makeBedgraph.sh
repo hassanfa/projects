71e32f4d4fe41e977a1aea6760c77aa293dc76ab
Hassan Foroughi Asl <hassanfa@rackham1.uppmax.uu.se>
2017-05-08 14:35:29 +0200

    _S

---
+++ snic2017-7-7/scripts_mRNA/3.makeBedgraph.sh
@@ -0,0 +1,51 @@
+#/bin/bash
+#Hassan May 2017
+#Create bedgraph for UCSC
+
+module load bioinfo-tools
+module load BEDTools
+
+cat SampleSheet_mRNA.tsv \
+  | awk -v OFS="\t" '{if ($1~/_[1,2]R_/)
+                        Side="Right";
+                      else Side="Left";
+                      if($1~/_1[R,L]_/)
+                        Stage="Stage1\t255,30,30\t155,30,30";
+                      else Stage="Stage2\t30,30,255\t30,30,155";
+                      print $0,Side,Stage}' > SampleSheet_mRNA_color.tsv
+
+while read -r name i Side Stage Colp Coln
+do
+  echo $i
+  for s in "-" "+"
+  do
+    trackCol=${Colp}
+    fname="plus"
+    if [ $s == "-" ]
+    then
+      trackCol=${Coln}
+      fname="neg"
+    fi
+
+    trackTitle=`echo -e "'name=\"${name}${s}\" visibility=4 color=${trackCol} description=\"${name}\"'"`
+
+    eval bedtools genomecov \
+      -ibam AE1_${i}_L008_R1_001/AE1_${i}_L008_R1_001.bam \
+      -bg \
+      -split \
+      -strand ${s} \
+      -trackline \
+      -trackopts ${trackTitle} \
+    | awk -v OFS="\t" '$0~/chr[0-9]/ || $0~/track/ {if ($4>1) print $0}' \
+    > AE1_${i}_L008_R1_001.bam.${fname}.bg.split
+    
+    eval bedtools genomecov \
+      -ibam AE1_${i}_L008_R1_001/AE1_${i}_L008_R1_001.bam \
+      -bg \
+      -split \
+      -trackline \
+      -trackopts ${trackTitle} \
+    | awk -v OFS="\t" '$0~/chr[0-9]/ || $0~/track/ {if ($4>1) print $0}' \
+    > AE1_${i}_L008_R1_001.bam.all.bg.split
+  done
+done < SampleSheet_mRNA_color.tsv
